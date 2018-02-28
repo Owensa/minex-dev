7dcdc57842ca687640e9fb226626b53e8cd8b460
andrewowens <andrew.tensor@gmail.com>
2018-02-28 12:37:44 -0500

    footer edit 2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span><a href="http://coreui.io">MinEX</a> &copy; 2018 MINEMY.</span>
+        <span><a href="http://mineq.co">MinEX</a> &copy; 2018 MINEMY.</span>
       </footer>
     )
   }
